7a8934fb22f27433bcb593ac872822b0db49cf27
Mohameed0 <88552256+Mohameed0@users.noreply.github.com>
2021-08-20 00:20:38 +0100

    Add files via upload

---
+++ Dom-check point/Dom check point.js
@@ -0,0 +1,53 @@
+// var carts=document.querySelectorAll("#add-btn")
+// console.log("haahha")
+
+// for (var i=0 ; i<carts.length ; i++){
+//     console.log('hahha')
+// }
+function like(heartbtn){
+    if(heartbtn.classList.contains("far")){
+        heartbtn.classList.remove("far");
+        heartbtn.classList.add("fas");
+    }else{
+        heartbtn.classList.remove("fas");
+        heartbtn.classList.add("far");
+    }
+}
+
+function addToCart(item){
+    
+        item.classList.remove("hiden")
+        if (item==document.getElementById('smart')){
+            document.getElementById("in1").value="1"
+        }
+        else if (item==document.getElementById('bleu_chanel')){
+            document.getElementById("in2").value="1"
+        }
+        else if (item==document.getElementById('sauvage')){
+            document.getElementById("in3").value="1"
+        }
+        else if (item==document.getElementById('hugo_boss')){
+            document.getElementById("in4").value="1"
+        }
+        else if (item==document.getElementById('dior_sport')){
+            document.getElementById("in5").value="1"
+        }
+        else if (item==document.getElementById('dolce_gabanna')){
+            document.getElementById("in5").value="1"
+        }
+        update(sum)
+}
+function removecart(item){
+    
+    item.classList.add("hiden")
+    if (item==document.getElementById('smart')){
+        document.getElementById("in1").value=0
+    }
+    update(sum)
+}
+var sum=0
+
+function update(tatal){
+    tatal+=(parseFloat(document.getElementById("in1").value)*55)+(parseFloat(document.getElementById("in2").value)*100)+(parseFloat(document.getElementById("in3").value)*250)+(parseFloat(document.getElementById("in4").value)*300)+(parseFloat(document.getElementById("in5").value)*200)+(parseFloat(document.getElementById("in6").value)*215)
+    document.getElementById("Total").innerHTML=tatal
+}
